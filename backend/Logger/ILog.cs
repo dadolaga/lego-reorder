6b8164351e59705e0554c15cbc7a2c82ce85761d
Davide Lagazio <lagazio.davide.1f@gmail.com>
2025-09-25 18:03:38 +0200

    Create logger project

---
+++ backend/Logger/ILog.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyLogger {
+    public interface ILog {
+        public static abstract void Init();
+
+        public static abstract void Fatal(string text, Exception? exception = null);
+
+        public static abstract void Error(string text, Exception? exception = null);
+
+        public static abstract void Warning(string text, Exception? exception = null);
+        
+        public static abstract void Information(string text);
+
+        public static abstract void Debug(string text);
+    }
+}
---
+++ backend/Logger/Log.cs
@@ -0,0 +1,55 @@
+﻿
+using Serilog;
+
+namespace MyLogger {
+    public class Log : ILog {
+        private static bool init_ = false;
+
+        public static void Init() {
+            Serilog.Log.Logger = new LoggerConfiguration()
+            .MinimumLevel.Debug()
+            .WriteTo.Console(
+                outputTemplate: "[{Timestamp:yyyy-MM-dd HH:mm:ss.fff} {Level:u4}] {Message:lj}{NewLine}{Exception}"
+            )
+            .CreateLogger();
+
+            init_ = true;
+        }
+
+        public static void Debug(string text) {
+            if(!init_)
+                return;
+
+            Serilog.Log.Debug(text);
+        }
+
+        public static void Error(string text, Exception? exception = null) {
+            if (!init_)
+                return;
+
+            Serilog.Log.Error(text, exception);
+        }
+
+        public static void Fatal(string text, Exception? exception = null) {
+            if (!init_)
+                return;
+
+            Serilog.Log.Fatal(text, exception);
+        }
+
+        public static void Information(string text) {
+            if (!init_)
+                return;
+
+            Serilog.Log.Information(text);
+        }
+
+        public static void Warning(string text, Exception? exception = null) {
+            if (!init_)
+                return;
+
+            Serilog.Log.Warning(text, exception);
+        }
+
+    }
+}
