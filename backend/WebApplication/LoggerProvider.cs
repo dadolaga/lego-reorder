29a29e4887666b6331b78e6de90cd8c85cd5da4c
Davide Lagazio <lagazio.davide.1f@gmail.com>
2025-09-25 18:03:56 +0200

    Create web application project

---
+++ backend/WebApplication/LoggerProvider.cs
@@ -0,0 +1,44 @@
+﻿namespace WebApplication {
+    public class LoggerProvider : ILoggerProvider {
+        public ILogger CreateLogger(string categoryName) {
+            return new Logger();
+        }
+
+        public void Dispose() {
+            
+        }
+    }
+
+    public class Logger : ILogger {
+        public IDisposable? BeginScope<TState>(TState state) where TState : notnull { 
+            return null;
+        }
+
+        public bool IsEnabled(LogLevel logLevel) {
+            return true;
+        }
+
+        public void Log<TState>(LogLevel logLevel, EventId eventId, TState state, Exception? exception, Func<TState, Exception?, string> formatter) {
+            switch (logLevel) {
+                case LogLevel.Critical:
+                    MyLogger.Log.Fatal(formatter(state, exception));
+                    break;
+                case LogLevel.Error:
+                    MyLogger.Log.Error(formatter(state, exception));
+                    break;
+                case LogLevel.Warning:
+                    MyLogger.Log.Warning(formatter(state, exception));
+                    break;
+                case LogLevel.Information:
+                    MyLogger.Log.Information(formatter(state, exception));
+                    break;
+                case LogLevel.Debug:
+                    MyLogger.Log.Debug(formatter(state, exception));
+                    break;
+                default:
+                    MyLogger.Log.Information(formatter(state, exception));
+                    break;
+            }
+        }
+    }
+}
---
+++ backend/WebApplication/Program.cs
@@ -0,0 +1,38 @@
+
+using MyLogger;
+using Serilog;
+
+namespace WebApplication {
+    public class Program {
+        public static void Main(string[] args) {
+            MyLogger.Log.Init();
+
+            var builder = Microsoft.AspNetCore.Builder.WebApplication.CreateBuilder(args);
+
+            // Add services to the container.
+
+            builder.Services.AddControllers();
+            // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+            builder.Services.AddEndpointsApiExplorer();
+            builder.Services.AddSwaggerGen();
+
+            builder.Logging.ClearProviders();
+            builder.Logging.AddProvider(new LoggerProvider());
+
+            var app = builder.Build();
+
+            // Configure the HTTP request pipeline.
+            if (app.Environment.IsDevelopment()) {
+                app.UseSwagger();
+                app.UseSwaggerUI();
+            }
+
+            app.UseAuthorization();
+
+
+            app.MapControllers();
+
+            app.Run();
+        }
+    }
+}
