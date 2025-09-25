a7c9f249f7fabb0ac8fcce7dcaf46dc118b1e1df
Davide Lagazio <lagazio.davide.1f@gmail.com>
2025-09-25 20:22:17 +0200

    Create database project

---
+++ backend/Database/DatabaseInitializer.cs
@@ -0,0 +1,23 @@
+﻿using System.Text.Json;
+
+namespace Database {
+    internal class JsonInitializer {
+        public string ConnectionString { get; set;}
+
+    }
+
+    public class DatabaseInitializer {
+        public static void ReadFromJson() {
+            string jsonPath = Path.Combine(Path.GetDirectoryName(Directory.GetCurrentDirectory()), "appsettings.json");
+
+            JsonInitializer? json = JsonSerializer.Deserialize<JsonInitializer>(File.ReadAllText(jsonPath));
+
+            if(json == null) { 
+                MyLogger.Log.Fatal("Database json initializer file not read");
+                return;
+            }
+
+            LegoDbContext.ConnectionString = json.ConnectionString;
+        }
+    }
+}
---
+++ backend/Database/LegoDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Database {
+    public class LegoDbContext : DbContext {
+        internal static string ConnectionString { set; private get; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder) {
+            optionsBuilder.UseMySql(connectionString: ConnectionString, ServerVersion.AutoDetect(ConnectionString));
+        }
+    }
+}
