14164af8a2e2a214c3969d8049ae545597b7678d
Davide Lagazio <lagazio.davide.1f@gmail.com>
2026-01-11 18:22:59 +0100

    Add docker for frontend

--- frontend/src/utilities/axios.ts
+++ frontend/src/utilities/axios.ts
@@ -1,6 +1,8 @@
 import axios from "axios";
 
+const baseURL = process.env.NODE_ENV === "production" ? "http://localhost:5000/" : "http://localhost:5196/";
+
 export const axiosInstance = axios.create({
-    baseURL: "http://localhost:5196/",
+    baseURL: baseURL,
     timeout: 1000
 })
