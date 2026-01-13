5df1f28e2ad5d9778ec88f8f29e1a6e44100d9e9
Davide Lagazio <lagazio.davide.1f@gmail.com>
2026-01-13 19:00:17 +0100

    Refactor base URL logic for axios instance to improve clarity and maintainability

--- frontend/src/utilities/axios.ts
+++ frontend/src/utilities/axios.ts
@@ -1,6 +1,18 @@
 import axios from "axios";
 
-const baseURL = process.env.NODE_ENV === "production" ? `http://${window.location.hostname}:5000/` : "http://localhost:5196/";
+const getBaseUrl = () => {
+    if (process.env.NODE_ENV === 'production') {
+        if (typeof window === 'undefined') {
+            return 'http://localhost:5000/';
+        } else {
+            return `http://${window.location.hostname}:5000/`;
+        }
+    }
+    
+    return 'http://localhost:5196/';
+}
+
+const baseURL = getBaseUrl();
 
 export const axiosInstance = axios.create({
     baseURL: baseURL,
